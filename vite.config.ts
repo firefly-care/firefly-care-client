d14d0061a99b9eee4b10b69f80102874a43a422e
jungsun <dlwjdtjs0111@gmail.com>
2025-08-12 13:50:48 +0900

    ✨ feat: recharts 사용 그래프 컴포넌트 구현

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
       "@apis": path.resolve(__dirname, "./src/apis"),
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@constants": path.resolve(__dirname, "./src/constants"),
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@types": path.resolve(__dirname, "./src/types"),
